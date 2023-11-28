ac8555d664beb6533fbc28326a9fd48fd15d14f9
Tomomi-K1 <tomomi.music@gmail.com>
2023-11-27 16:42:45 -0800

    pulling data from todoList

--- app/middleware.js
+++ app/middleware.js
@@ -1,6 +1,7 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
+// we need this middleware so that if the cookie is expired, this middleware will getSession and refresh cookies on server side also.
 export async function middleware(req){
     const res = NextResponse.next();
     const supabase = createMiddlewareClient({req, res});
--- app/page.js
+++ app/page.js
@@ -1,18 +1,33 @@
 // server side component
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers';
-import AuthButton from './auth-button';
+import Navbar from './auth-button';
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
-  const { data : notes } = await supabase.from("notes").select();
+  const { data : todos} = await supabase.from("todos").select();
+
   // console.log(JSON.stringify(notes, null, 2));
   return (
-    <>
-      <AuthButton />
-      <h1>TO Do APP</h1>
-      <p>{JSON.stringify(notes, null, 2)}</p>
-    </>
+    <div className='h-screen flex flex-col items-center pt-10'>
+      <main>
+        <h1 className='text-xl font-bold'>TO Do APP</h1>
+        <ul className='list-decimal'>
+        {todos.map((item, idx) =>( <TodoItem key = {idx} item = {item} />))}
+        </ul>
+      </main>
+    </div>
    
   )
+}
+
+
+function TodoItem({item}){
+  return (
+    <li className='flex'>
+      <h5 className='pr-2'>{item.task}</h5>
+      <p>{item.is_completed? "done": "Not completed"}</p>
+    </li>
+  )
+
 }
